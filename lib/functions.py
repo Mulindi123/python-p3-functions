f3d381e2c90c1dc158c8d9f9d27ba0be25c71e76
Mulindi123 <medrine.mulindi@gmail.com>
2023-09-12 16:55:06 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+   print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Medrine")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Mulindi")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(4,5)
 
 def halve(number):
-    pass
+    return number/2
+halve(18)
